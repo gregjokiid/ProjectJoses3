8c39a96401cbb99faf2954bf8fc59f8f775af119
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-07-10 21:18:26 +0700

    create profile fragment

--- MCSJoses/app/src/main/java/com/example/mcsjoses/ProfileFragment.java
+++ MCSJoses/app/src/main/java/com/example/mcsjoses/ProfileFragment.java
@@ -2,13 +2,23 @@ package com.example.mcsjoses;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class ProfileFragment extends Fragment {
+    EditText etToken;
+
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -18,5 +28,26 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        etToken = view.findViewById(R.id.etToken);
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            System.out.println("Fetching FCM registration token failed");
+                            return;
+                        }
+                        String token = task.getResult();
+                        System.out.println(token);
+                        Toast.makeText(getContext(), "Your device registration token is" + token, Toast.LENGTH_SHORT).show();
+                        etToken.setText(token);
+                    }});
     }
 }
